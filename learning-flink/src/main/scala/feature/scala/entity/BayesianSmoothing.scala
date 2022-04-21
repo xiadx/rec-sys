f93c416f9524a04c03296ce9781d8c0fafc3eadd
xiadx <1463078920@qq.com>
2022-04-22 00:28:23 +0800

    deleted learning-flink

--- learning-flink/src/main/scala/feature/scala/entity/BayesianSmoothing.scala
+++
@@ -1,7 +0,0 @@
-package feature.scala.entity
-
-case class BayesianSmoothing(version: String,
-                             itemType: String,
-                             pageScene: String,
-                             alpha: Double,
-                             beta: Double)
--- learning-flink/src/main/scala/feature/scala/entity/OnlineFeature.scala
+++
@@ -1,6 +0,0 @@
-package feature.scala.entity
-
-case class OnlineFeature(d_val: String,
-                         fea_type: String,
-                         fea_name: String,
-                         expression: String)
